4e7213837a4e3078ed8dcf3c49719ae30dcae576
Carlos Lopez Perez <iamlowlo@gmail.com>
2016-04-10 14:21:47 +0200

    Added date.service.js to format dateTimes

---
+++ src/app/components/date/date.service.js
@@ -0,0 +1,26 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('angularProject')
+		.service('dateService', DateService);
+
+	function DateService() {
+		this.getFormattedTime = function(date){
+			return 	date.getDate()
+					+'/'+(date.getMonth()+1)
+					+'/'+date.getFullYear()
+					+' '+date.getHours()
+					+':'+this.twoDigitsValue(date.getMinutes())
+					+':'+this.twoDigitsValue(date.getSeconds());
+		};
+
+		this.twoDigitsValue = function(value){
+			if (value.toString().length<2) {
+				return '0'+value;
+			}else{
+				return value;
+			}
+		}
+	}
+})();
